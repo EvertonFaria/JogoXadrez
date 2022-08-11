814486d3d0195eb078cd5cfd13a014f1de5aaef6
Everton Faria de Oliveira <everton.faria@outlook.com>
2022-08-11 00:50:36 -0300

    Excessões e programação defensiva

---
+++ src/jogotabuleiro/ExcessoesTabuleiro.java
@@ -0,0 +1,9 @@
+package jogotabuleiro;
+
+public class ExcessoesTabuleiro extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ExcessoesTabuleiro(String msg) {
+		super(msg);
+	}
+}
